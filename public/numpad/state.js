75d40a4e91ee4a1ea8310518f27c0ff956373da2
Md Abdulla <iamabdullah73524@gmail.com>
2026-06-11 20:26:02 +0530

    fix numpad

--- public/numpad/state.js
+++ public/numpad/state.js
@@ -1,23 +1,23 @@
-const state = {
-  entries: []
-};
+// const state = {
+//   entries: []
+// };
 
-export const getEntries = () => [...state.entries];
+// export const getEntries = () => [...state.entries];
 
-export const addEntry = (entry) => {
-  state.entries.push(entry);
-};
+// export const addEntry = (entry) => {
+//   state.entries.push(entry);
+// };
 
-export const isDuplicate = (value) => {
-  return state.entries.some((e) => e.value === value);
-};
+// export const isDuplicate = (value) => {
+//   return state.entries.some((e) => e.value === value);
+// };
 
-export const removeEntry = (value) => {
-  const index = state.entries.findIndex(
-    (e) => e.value === value
-  );
+// export const removeEntry = (value) => {
+//   const index = state.entries.findIndex(
+//     (e) => e.value === value
+//   );
 
-  if (index !== -1) {
-    state.entries.splice(index, 1);
-  }
-};
+//   if (index !== -1) {
+//     state.entries.splice(index, 1);
+//   }
+// };
