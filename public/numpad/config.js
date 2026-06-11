7d6188c2cb95f9440b3b4795c488e2b7e1d9d965
Md Abdulla <iamabdullah73524@gmail.com>
2026-06-11 21:05:28 +0530

    Revert "fix numpad"
    This reverts commit 75d40a4e91ee4a1ea8310518f27c0ff956373da2.

--- public/numpad/config.js
+++ public/numpad/config.js
@@ -1,10 +1,10 @@
-// export const CONFIG = {
-//   MAX_LENGTH: 16,
-//   ALLOW_DUPLICATES: false,
+export const CONFIG = {
+  MAX_LENGTH: 16,
+  ALLOW_DUPLICATES: false,
 
-//   CURRENT_USER: {
-//     id: 1,
-//     name: "Abdul",
-//     role: "admin"
-//   }
-// };
+  CURRENT_USER: {
+    id: 1,
+    name: "Abdul",
+    role: "admin"
+  }
+};
--- public/numpad/utils.js
+++ public/numpad/utils.js
@@ -1,13 +1,13 @@
-// export const isValidInput = (value, maxLength) => {
-//   const regex = /^[0-9]{1,16}$/;
-//   return regex.test(value) && value.length <= maxLength;
-// };
+export const isValidInput = (value, maxLength) => {
+  const regex = /^[0-9]{1,16}$/;
+  return regex.test(value) && value.length <= maxLength;
+};
 
-// export const getDateTime = () => {
-//   const now = new Date();
+export const getDateTime = () => {
+  const now = new Date();
 
-//   return {
-//     date: now.toLocaleDateString(),
-//     time: now.toLocaleTimeString()
-//   };
-// };
+  return {
+    date: now.toLocaleDateString(),
+    time: now.toLocaleTimeString()
+  };
+};
